63b6d88a134469878fa11a6c832a41a0a0402809
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-04-11 14:46:34 -0300

    fixes transfer

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -5,12 +5,13 @@ class Transfer extends Component {
   transfer = (e) => {
     e.preventDefault();
     const token = this.token.value;
-    const to = this.to.value
+    const to = web3.isAddress(this.to.value) ? this.to.value : false;
     const amount = this.amount.value
 
-    this.props.transferToken(token, to, amount);
-
-    this.transferForm.reset();
+    if (token && to && amount) {
+      this.props.transferToken(token, to, amount);
+      this.transferForm.reset();
+    }
   }
 
   render() {
@@ -23,23 +24,17 @@ class Transfer extends Component {
           <div className="row">
             <div className="col-md-12">
               <div>
-                <form ref={(input) => this.transferForm = input} onSubmit={(e) => this.transfer(e)}>
-                  <div>
-                    <label>Token</label>
-                    <select ref={(input) => this.token = input} >
-                      <option value="gem">GEM</option>
-                      <option value="skr">SKR</option>
-                      <option value="sai">SAI</option>
-                    </select>
-                  </div>
-                  <div>
-                    <label>To</label>
-                    <input ref={(input) => this.to = input} type="text" placeholder="0x" />
-                  </div>
-                  <div>
-                    <label>Amount</label>
-                    <input ref={(input) => this.amount = input} type="number" placeholder="0.00" />
-                  </div>
+                <form className="transfer" ref={(input) => this.transferForm = input} onSubmit={(e) => this.transfer(e)}>
+                  <label>Token</label>
+                  <select ref={(input) => this.token = input} >
+                    <option value="gem">GEM</option>
+                    <option value="skr">SKR</option>
+                    <option value="sai">SAI</option>
+                  </select>
+                  <label>To</label>
+                  <input ref={(input) => this.to = input} type="text" placeholder="0x" />
+                  <label>Amount</label>
+                  <input ref={(input) => this.amount = input} type="number" placeholder="0.00" step="0.001" />
                   <input type="submit" />
                 </form>
               </div>
