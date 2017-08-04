17d5ecfd22762fca49add10a1df69654edb224c5
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-08-04 16:20:50 -0300

    deploy scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var fs       = require('fs');
 var gulp     = require('gulp');
 var gulpsync = require('gulp-sync')(gulp)
 var gutil    = require('gulp-util');
+var ghPages  = require('gulp-gh-pages')
+var surge    = require('gulp-surge')
 
 var LIB_PATH = 'lib/sai/';
 var LIB_OUT  = LIB_PATH + 'out/';
@@ -54,4 +56,18 @@ gulp.task('generate', (cb) => {
     fs.writeFileSync(`${OUT}${file.toLowerCase()}.json`, JSON.stringify(out, null, 2));
     gutil.log(`Wrote to ${OUT}${file.toLowerCase()}.json`);
   });
+})
+
+// gh-pages
+gulp.task('deploy-gh-pages', function () {
+  require('fs').writeFileSync('./build/CNAME', 'sai.makerdao.com');
+  return gulp.src('./build/**/*')
+    .pipe(ghPages())
+})
+
+gulp.task('deploy-surge', [], function () {
+  return surge({
+    project: './build',         // Path to your static build directory
+    domain: 'https://sai-dev.surge.sh'  // Your domain or Surge subdomain
+  })
 })
